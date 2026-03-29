127c11c6f4bbdbd7358afdff9aaf87bf62b1805d
Tallarin51 <ignacio.pasten01@alumnos.ucn.cl>
2026-03-29 01:04:31 -0300

    add registrarActividad

--- Taller1/logica/Main.java
+++ Taller1/logica/Main.java
@@ -6,6 +6,8 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		
+		//Carga los usuarios y registros desde archivos
+		
 		String[] listaNombre = new String[3];
 		String[] contraseñas = new String[3];
 		
@@ -62,6 +64,7 @@ public class Main {
 	}
 	
 	public static void  mostrarMenu(String[] listaActividades, String[] listaUser, String[] listaFechas, int[] cantHoras,String[] listaNombre, String[] contraseñas) {
+		//Muestra los menus disponibles 
 		String opcion;
 		Scanner s = new Scanner(System.in);
 		do {
@@ -93,6 +96,7 @@ public class Main {
 	}
 
 	private static void mostrarMenuAnalisis(String[] listaActividades, String[] listaUser, String[] listaFechas, int[] cantHoras) {
+		//Muestra el menu de analisis y lo que puede hacer cada una de sus opciones
 		String opcion;
 		Scanner s = new Scanner(System.in);
 		
@@ -136,6 +140,8 @@ public class Main {
 
 	private static void mostrarMasRealizada(String[] actividades) {
 		
+		//Muestra la actividad que mas se realizo y cuantas veces se realizo
+		
 		String masRepetida = " ";
 		int mayorCant = 0;
 		
@@ -172,6 +178,8 @@ public class Main {
 	}
 		
 	private static void masRealizadaPorUsuario(String[] actividades, String[] usuarios,int[] cantHoras) {
+		
+		//Muestra la actividad mas realizada por cada uno de los usuarios y cuantas horas duro contando todas
 		
 		for (int i = 0; i < usuarios.length; i++) {
 			
@@ -239,6 +247,8 @@ public class Main {
 	
 	private static void masProcrastinacion(String[] usuarios, int[] cantHoras) {
 		
+		//Muestra al usuario mas procrastinador y cuantas horas gasto en total
+		
 		String usuarioMayor = " ";
 		int mayorHoras = 0;
 		
@@ -287,7 +297,9 @@ public class Main {
 	}
 	
 	private static void todasActividades(String[] listaActividades, String[] listaUser, String[] listaFechas, int[] cantHoras) {
-
+		
+	//Muestra todas las lineas del archivo pero de una forma mas bonita
+		
 	System.out.println("Lista de actividades: ");
 	
 	for (int i = 0; i < listaActividades.length; i ++) {
@@ -309,6 +321,9 @@ public class Main {
 	
 	private static void mostrarMenuUsuarios(String[] listaUser, String[] listaNombre, String[] contraseñas,
                                         String[] listaActividades, String[] listaFechas, int[] cantHoras) {
+		
+		//Muestra el menu de usuarios y las opciones disponibles para usar
+		
 		Scanner s = new Scanner(System.in);
 		System.out.println("Usuario: ");
 		String user = s.nextLine();
@@ -319,7 +334,7 @@ public class Main {
 		boolean acceso = false;
 		
 		for(int i = 0; i < listaNombre.length; i++) {
-			if (listaNombre[i].equals(user) && contraseñas[i].equals(pass)) {
+			if (listaNombre[i] != null && contraseñas[i] != null && listaNombre[i].equals(user) && contraseñas[i].equals(pass)) {
 				acceso = true;
 				break;
 			}
@@ -353,13 +368,26 @@ public class Main {
 					}
 					break;
 				case "2":
-					modificarActividad();
+					try {
+						modificarActividad(user, listaUser, listaFechas, cantHoras, listaActividades);
+					} catch (IOException e) {
+						System.out.println("Error al modificar");
+					}
 					break;
 				case "3":
-					eliminarActividad();
+					try {
+					eliminarActividad(user, listaUser, listaFechas, cantHoras, listaActividades);
+						
+					} catch (IOException e) {
+						System.out.println("Error al eliminar");
+					}
 					break;
 				case "4":
-					cambiarContraseña();
+					try {
+						cambiarContraseña(user, listaNombre, contraseñas);
+					} catch (IOException e) {
+						System.out.println("Error al cambiar contraseña");
+					}
 					break;
 				case "5":
 					System.out.println("Saliendo...");
@@ -376,23 +404,228 @@ public class Main {
 
 	private static void registrarActividad(String user, String[] listaUser, String[] listaFechas, int[] cantHoras, String[] listaActividades) throws IOException {
 		
+		//Registra una actividad (añade) y modifica el txt
 		
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println("Ingrese fecha: ");
+		String fecha = s.nextLine();
+		
+		System.out.println("Ingrese cantidad de horas: ");
+		int horas = Integer.parseInt(s.nextLine());
+		
+		System.out.println("Ingrese actividad: ");
+		String actividad = s.nextLine();
+		
+		for (int i = 0; i < listaUser.length; i++) {
+			
+			if (listaUser[i] == null) {
+				listaUser[i] = user;
+				listaFechas[i] = fecha;
+				cantHoras[i] = horas;
+				listaActividades[i] = actividad;
+				break;
+			}
+			
+		}
+		
+		FileWriter fw = new FileWriter("Registros.txt", true);
+		BufferedWriter bw = new BufferedWriter(fw);
+		
+		bw.write(user + ";" + fecha + ";" + horas + ";" + actividad);
+		bw.newLine();
+		
+		bw.close();
+		
+		System.out.println("Actividad registrada correctamente");
 		
 		
 	}
-	private static void modificarActividad() {
-		// TODO Auto-generated method stub
+	private static void actualizarArchivo(String[] listaUser, String[] listaFechas, int[] cantHoras, String[] listaActividades) throws IOException {
+		
+		//Reescribe el archivo de registros actualizado
+		
+		BufferedWriter bw = new BufferedWriter(new FileWriter("Registros.txt")); 
+		
+		for (int i = 0; i < listaUser.length;i++) {
+			if (listaUser[i] != null) {
+				
+				bw.write(listaUser[i] + ";" + listaFechas[i] + ";" + cantHoras[i] + ";" + listaActividades[i]);
+				bw.newLine();
+			}
+		}
+		
+		bw.close();
 		
 	}
-	private static void eliminarActividad() {
-		// TODO Auto-generated method stub
+	private static void modificarActividad(String user, String[] listaUser, String[] listaFechas, int[] cantHoras, String[] listaActividades) throws IOException  {
 		
-	}
-	private static void cambiarContraseña() {
-		// TODO Auto-generated method stub
+		//Modifica una actividad ya existente y podemos modificar tanto fecha, duracion o actividad
 		
-	}
+		Scanner s = new Scanner(System.in);
+		System.out.println("Cual actividad deseas modificar?");
+		System.out.println("0) Regresar.");
+		
+		for (int i = 0; i < listaUser.length; i++) {
+			
+			if (listaUser[i] != null && listaUser[i].equals(user)) {
+				System.out.println(i + ")" + listaUser[i] + ";" + listaFechas[i] + ";" + cantHoras[i] + ";" + listaActividades[i]);
+			}
+		}
+		int posicion;
+		try {
+		posicion = Integer.parseInt(s.nextLine());
+		} catch (NumberFormatException e) {
+			System.out.println("Debe ingresar número válido");
+			return;
+		}
+		if (posicion < 0 || posicion >= listaUser.length) {
+			System.out.println("Índice fuera de rango");
+			return;
+		}
+		if (posicion == 0) {
+			return;
+		}
+		
+		if (listaUser[posicion] == null || !listaUser[posicion].equals(user)) {
+			System.out.println("Opción inválida");
+			return;
+		}
+		
+		int opcion = -1;
+		
+		do {
+			System.out.println("Que deseas modificar?\r\n"
+					+ "\r\n"
+					+ "0) Regresar.\r\n"
+					+ "1) Fecha\r\n"
+					+ "2) Duracion\r\n"
+					+ "3) Tipo de actividad");
 
+			try {
+			opcion = Integer.parseInt(s.nextLine());
+			} catch (NumberFormatException e) {
+				System.out.println("Ingrese un número válido");
+				continue;
+			}
+			switch (opcion) {
+				case 1:
+					System.out.println("0) Regresar.");
+					System.out.println("Ingrese nueva fecha: ");
+					String nuevaFecha = s.nextLine();
+					
+					if (!nuevaFecha.equals("0")) {
+						listaFechas[posicion] = nuevaFecha;
+					}
+					break;
+			
+				case 2:
+					System.out.println("0) Regresar.");
+					System.out.println("Ingrese nueva duración: ");
+					String nuevaDuracion = s.nextLine();
+					
+					if (!nuevaDuracion.equals("0")) {
+						cantHoras[posicion] = Integer.parseInt(nuevaDuracion);
+					}
+					break;
+					
+				case 3:
+					System.out.println("0) Regresar.");
+					System.out.println("Ingrese nueva actividad: ");
+					String nuevaActividad = s.nextLine();
+					
+					if (!nuevaActividad.equals("0")) {
+						listaActividades[posicion] = nuevaActividad;
+					}
+					break;
+			}
+
+		} while (opcion != 0);
+		
+		System.out.println("Operación finalizada");
+		
+		actualizarArchivo(listaUser, listaFechas, cantHoras, listaActividades);
+		
+	}
+	private static void eliminarActividad(String user, String[] listaUser, String[] listaFechas, int[] cantHoras, String[] listaActividades) throws IOException  {
+		
+		//Permite eliminar una actividad ya existente y lo modifica en el archivo
+		
+		Scanner s = new Scanner(System.in);
+		System.out.println("Cual actividad deseas eliminar?");
+		System.out.println("0) Regresar.");
+		
+		for (int i = 0; i < listaUser.length; i++) {
+			
+			if (listaUser[i] != null && listaUser[i].equals(user)) {
+				System.out.println(i + ")" + listaUser[i] + ";" + listaFechas[i] + ";" + cantHoras[i] + ";" + listaActividades[i]);
+			}
+		}
+		
+		int posicion = Integer.parseInt(s.nextLine());
+		if (posicion == 0) {
+			System.out.println("Operación cancelada");
+			return;
+		}
+		if (listaUser[posicion] == null || !listaUser[posicion].equals(user)) {
+			System.out.println("Opción inválida");
+			return;
+		}
+		
+		listaUser[posicion] = null;
+		listaFechas[posicion] = null;
+		cantHoras[posicion] = 0;
+		listaActividades[posicion] = null;
+		
+		System.out.println("Actividad eliminada correctamente");
+		
+		actualizarArchivo(listaUser, listaFechas, cantHoras, listaActividades);
+		
+	}
+	private static void cambiarContraseña(String user, String[] listaNombre, String[] contraseñas) throws IOException {
+		
+		//Permite modificar una contraseña mientras que sepa la contraseña anterior y la modifica en el archivo
+		
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println("Ingrese contraseña actual: ");
+		String actual = s.nextLine();
+		
+		int posicion = -1;
+		
+		for (int i = 0; i < listaNombre.length; i++) {
+			if (listaNombre[i] != null && listaNombre[i].equals(user)) {
+				posicion = i;
+				break;
+			}
+		}
+		
+		if (posicion == -1 || !contraseñas[posicion].equals(actual)) {
+			System.out.println("Contraseña incorrecta");
+			return;
+		}
+		
+		System.out.println("Ingrese nueva contraseña: ");
+		String nueva = s.nextLine();
+		
+		contraseñas[posicion] = nueva;
+		
+		BufferedWriter bw = new BufferedWriter(new FileWriter("Usuarios.txt"));
+		
+		for (int i = 0; i < listaNombre.length;i++) {
+			
+			if (listaNombre[i] != null) {
+				
+				bw.write(listaNombre[i] + ";" + contraseñas[i]);
+				bw.newLine();
+			}
+		}
+		
+		bw.close();
+		
+		System.out.println("Contraseña cambiada correctamente");
+		
+	}
 
 
 	
